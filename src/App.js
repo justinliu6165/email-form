2722781994ea4a0a6cc250d09694b266e2219921
Justin Liu <justin_liu@hotmail.co.uk>
2018-12-18 22:42:26 +0000

    form added to frontend

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  state = {
+    name: '',
+    email: '',
+    message: ''
+  }
+
+  handleChange = (e) => {
+    this.setState({ [e.target.name]: e.target.value });
+  }
+  handleSubmit = (e) => {
+    e.preventDefault();
+
+    const { name, email, message } = this.state;
+
+    fetch('/api/form', {
+      method: 'POST',
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        name, email, message
+      })
+    })
+    this.resetForm();
+  }
+
+  resetForm = () => this.setState({ name: '', email: '', message: '' })
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <h1 className='form__title'>Email Form</h1>
+        <form onSubmit={(e) => this.handleSubmit(e)} className='form'>
+          <label className='form__label'>Please fill in the fields...</label>
+          <input type='text' placeholder='Name...' name='name' className='form__input' value={this.state.name} onChange={(e) => this.handleChange(e)} />
+          <input type='email' placeholder='Email...' name='email' className='form__input' value={this.state.email} onChange={(e) => this.handleChange(e)} />
+          <textarea type='text' placeholder='Message...' name='message' className='form__input' value={this.state.message} onChange={(e) => this.handleChange(e)} />
+          <button type='submit' className='form__btn'>Send a message</button>
+        </form>
       </div>
     );
   }
